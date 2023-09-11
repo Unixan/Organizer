fc11c4262c00c5b5a66ba427da4de762f0e48af2
Ståle <staale_80@hotmail.com>
2023-09-11 18:54:40 +0200

    Day over

--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -11,7 +11,10 @@ export const CreateAccount = () => {
 
   const newUser = z
     .object({
-      email: z.string().email({ message: "Ugyldig Email adresse" }),
+      email: z
+        .string()
+        .toLowerCase()
+        .email({ message: "Ugyldig Email adresse" }),
       screenName: z
         .string()
         .min(3, { message: "Navn må inneholde minst 3 tegn" }),
@@ -45,75 +48,68 @@ export const CreateAccount = () => {
         <h1 className="py-10 top-16 text-center text-white text-4xl font-bold drop-shadow-lg">
           Opprett ny bruker
         </h1>
-        <div className="flex flex-col gap-4 font-bold">
-          <div className="flex flex-col items-center">
-            <input
-              {...register("email")}
-              className={`rounded-full p-3 w-4/6 max-w-[250px] h-12 ${
-                errors.email
-                  ? "border-red-600 border-[5px]"
-                  : "border-blue-800 border-4"
-              }`}
-              id="Email"
-              type="text"
-              placeholder="E-mail adresse ..."
-              autoComplete="on"
-            />
-            {errors.email && (
-              <p className="text-red-600">{errors.email.message}</p>
-            )}
-          </div>
-          <div className="flex flex-col items-center">
-            <input
-              {...register("screenName")}
-              className={`rounded-full p-3 w-4/6 max-w-[250px] h-12 ${
-                errors.screenName
-                  ? "border-red-600 border-[5px]"
-                  : "border-blue-800 border-4"
-              }`}
-              id="Name"
-              type="text"
-              placeholder="Skjermnavn ..."
-              autoComplete="on"
-            />
-            {errors.screenName && (
-              <p className="text-red-600">{errors.screenName.message}</p>
-            )}
-          </div>
-          <div className="flex flex-col items-center">
-            <input
-              {...register("newPassword")}
-              className={`rounded-full p-3 w-4/6 max-w-[250px] h-12 ${
-                errors.newPassword
-                  ? "border-red-600 border-[5px]"
-                  : "border-blue-800 border-4"
-              }`}
-              id="Password"
-              type={isHidden ? "password" : "text"}
-              placeholder="Ønsket passord ..."
-              autoComplete="on"
-            />
-            {errors.newPassword && (
-              <p className="text-red-600">{errors.newPassword.message}</p>
-            )}
-          </div>
-          <div className="flex flex-col items-center">
-            <input
-              {...register("confirm")}
-              className={`rounded-full p-3 w-4/6 max-w-[250px] h-12 ${
-                errors.confirm
-                  ? "border-red-600 border-[5px]"
-                  : "border-blue-800 border-4"
-              }`}
-              id="Passwordcontrol"
-              type="password"
-              placeholder="Gjenta passord ..."
-              autoComplete="on"
-            />
-            {errors.confirm && (
-              <p className="text-red-600">{errors.confirm.message}</p>
-            )}
-          </div>
+        <div className="flex flex-col gap-2 font-bold items-center w-full">
+          <input
+            {...register("email")}
+            className={`rounded-full p-3 w-4/6 max-w-[250px] h-12 ${
+              errors.email
+                ? "border-red-600 border-[5px]"
+                : "border-blue-800 border-4"
+            }`}
+            id="Email"
+            type="text"
+            placeholder="E-mail adresse ..."
+            autoComplete="on"
+          />
+
+          <p className="text-red-600 h-[24px]">
+            {errors.email ? errors.email.message : ""}
+          </p>
+          <input
+            {...register("screenName")}
+            className={`rounded-full p-3 w-4/6 max-w-[250px] h-12 ${
+              errors.screenName
+                ? "border-red-600 border-[5px]"
+                : "border-blue-800 border-4"
+            }`}
+            id="Name"
+            type="text"
+            placeholder="Skjermnavn ..."
+            autoComplete="on"
+          />
+          <p className="text-red-600 h-[24px]">
+            {errors.screenName ? errors.screenName.message : ""}
+          </p>
+          <input
+            {...register("newPassword")}
+            className={`rounded-full p-3 w-4/6 max-w-[250px] h-12 ${
+              errors.newPassword
+                ? "border-red-600 border-[5px]"
+                : "border-blue-800 border-4"
+            }`}
+            id="Password"
+            type={isHidden ? "password" : "text"}
+            placeholder="Ønsket passord ..."
+            autoComplete="on"
+          />
+          <p className="text-red-600 h-[24px]">
+            {errors.newPassword ? errors.newPassword.message : ""}
+          </p>
+          <input
+            {...register("confirm")}
+            className={`rounded-full p-3 w-4/6 max-w-[250px] h-12 ${
+              errors.confirm
+                ? "border-red-600 border-[5px]"
+                : "border-blue-800 border-4"
+            }`}
+            id="Passwordcontrol"
+            type="password"
+            placeholder="Gjenta passord ..."
+            autoComplete="on"
+          />
+          <p className="text-red-600 h-[24px]">
+            {errors.confirm ? errors.confirm.message : ""}
+          </p>
         </div>
         <div className="grid grid-cols-2 justify-items-center gap-8 m-10 h-12">
           <button
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,48 +1,76 @@
+import { z } from "zod";
 import useCreateNew from "../hooks/createNew";
+import { FieldValues, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export const Login = () => {
+  const loginInfo = z.object({
+    login: z.string().toLowerCase().email({ message: "Ugyldig format" }),
+    password: z.string().min(8, { message: "Passord for kort" }),
+  });
+
+  type FormData = z.infer<typeof loginInfo>;
+
+  const onsSubmit = (data: FieldValues) => console.log(data);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(loginInfo) });
+
   const { setCreateNew } = useCreateNew();
 
   return (
-    <form onSubmit={(event) => event.preventDefault()}>
-      <h1 className="py-10 top-16 text-center text-white text-4xl font-bold drop-shadow-lg">
-        Velkommen!
-      </h1>
-      <div className="flex flex-col gap-10 h-screen">
-        <div className="flex flex-col gap-8">
-          <div className="flex flex-col items-center">
-            <input
-              className="rounded-full p-3 w-4/6 max-w-[250px] h-12 border-4 border-blue-800"
-              id="username"
-              type="email"
-              placeholder="Brukernavn (E-Mail) ..."
-              autoComplete="on"
-            />
-          </div>
-          <div className="flex flex-col items-center">
-            <input
-              className="rounded-full p-3 w-4/6 max-w-[250px] h-12 border-4 border-blue-800"
-              id="password"
-              type="text"
-              placeholder="Passord ..."
-              autoComplete="on"
-            />
-          </div>
-        </div>
-        <div className="flex justify-center flex-col">
-          <div className="flex justify-center h-12">
-            <button className="loginbutton border-4 w-44 bg-blue-600 border-blue-800 rounded-full text-white">
+    <div className="h-screen">
+      <form onSubmit={handleSubmit(onsSubmit)}>
+        <h1 className="py-10 top-16 text-center text-white text-4xl font-bold drop-shadow-lg">
+          Velkommen!
+        </h1>
+        <div className="flex flex-col font-bold gap-2 items-center">
+          <input
+            {...register("login")}
+            className={`rounded-full p-3 h-12 w-4/6 max-w-[250px] ${
+              errors.login
+                ? "border-red-600 border-[5px]"
+                : "border-blue-800 border-4"
+            }`}
+            id="username"
+            type="email"
+            placeholder="Brukernavn (Email) ..."
+            autoComplete="on"
+          />
+          <p className="text-red-600 h-[24px]">
+            {errors.login ? errors.login.message : ""}
+          </p>
+          <input
+            {...register("password")}
+            className={`rounded-full p-3 h-12 w-4/6 max-w-[250px] ${
+              errors.password
+                ? "border-red-600 border-[5px]"
+                : "border-blue-800 border-4"
+            }`}
+            id="password"
+            type="text"
+            placeholder="Passord ..."
+            autoComplete="off"
+          />
+          <p className="text-red-600 h-[24px]">
+            {errors.password ? errors.password.message : ""}
+          </p>
+          <div className="flex justify-center flex-col">
+            <button className="loginbutton border-4 w-44 place-self-center h-12 bg-blue-600 border-blue-800 rounded-full text-white">
               Logg inn
             </button>
-          </div>
-          <div className="text-white text-center pt-2">
-            Ingen konto? Opprett en{" "}
-            <a href="#" onClick={() => setCreateNew()} className="underline">
-              her
-            </a>
+            <div className="text-white text-center pt-2">
+              Ingen konto? Opprett en{" "}
+              <a href="#" onClick={() => setCreateNew()} className="underline">
+                her
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
